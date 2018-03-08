16f098ecb6b627d8d359f0710e71e468dead5983
Petrie van Zyl <petrie@codeforte.com>
2018-03-08 16:42:10 +0200

    new logo. transitions modified

--- src/containers/page-welcome.js
+++ src/containers/page-welcome.js
@@ -23,7 +23,7 @@ class PageWelcome extends Component {
 		// Logo
 		anime({
 			targets: '#logo-image',
-			opacity: [0, 0.35],
+			opacity: [0, 0.1],
 			delay: 1000,
 			duration: 4000,
 			easing: 'easeInQuart'
@@ -47,29 +47,68 @@ class PageWelcome extends Component {
 			duration: 3000,
 			easing: 'easeInOutQuart'
 		});
-		
-
 	}
 
-	// componentWillLeave() {
-	// 	console.log("WILL NOUTNMOUT");
-	// 	anime({
-	// 		targets: '.middle',
-	// 		opacity: [
-	// 			1, 0
-	// 		],
-	// 		duration: 2000,
-	// 
-	// 		loop: false
-	// 	});
-	// }
+	startExit() {
+		// Background
+		anime({
+			targets: '.page-welcome',
+			opacity: 0,
+			delay: 500,
+			duration: 2000,
+			easing: 'easeInQuart'
+		});
+	
+		// Logo
+		anime({
+			targets: '#logo-image',
+			opacity: 0,
+			duration: 1000,
+			easing: 'easeInQuart'
+		});
+		
+		// Text
+		anime({
+			targets: '.middle',
+			opacity: 0,
+			scaleY: 0,
+			easing: 'easeInOutQuart'
+		});
+		
+		// Biz Link
+		anime({
+			targets: '.biz-link',
+			opacity: 0,
+			easing: 'easeInOutQuart'
+		});
+		
+		// Wave
+		anime({
+			targets: '.wave-container .head',
+			top: -500,
+			duration: 2000,
+			easing: 'easeInOutQuart'
+		});
+		anime({
+			targets: '.wave-container .foot',
+			bottom: -500,
+			duration: 2000,
+			easing: 'easeInOutQuart'
+		});
+		
+		// setTimeout(this.routeToNextPage.bind(this), 2500);
+	}
+
+	routeToNextPage() {
+		console.log("ROOOT");
+	}
 
 	render() {
 		return (
 			<div className="page-welcome">
 
 				{/* ===== BACKGROUND LOGO ===== */}
-				<image id="logo-image" />
+				<div id="logo-image" />
 
 				{/* ===== TEXT CONTENT ===== */}
 				<div className="outer">
@@ -79,7 +118,11 @@ class PageWelcome extends Component {
 						</div>
 						<div className="inner main">
 							{/* // TODO: Add route for this */}
-							<p id="start-link" className="text-field sub">Start</p>
+							<p 
+								id="start-link" 
+								className="text-field sub"
+								onClick={this.startExit}
+								>Start</p>
 						</div>
 					</div>
 				</div>
